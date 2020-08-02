62289cdfff7d03a6fc4787f307b72cded99efdaa
developersam1995 <samrudh.kumar@yulu.bike>
2020-08-02 17:25:19 +0530

    add booking test

---
+++ lib/cab-service/booking_test.go
@@ -0,0 +1,60 @@
+package cs
+
+import "testing"
+
+type mockBookingsDb struct {
+	brs []BookingRequest
+}
+
+func newBookingsMockDb() *mockBookingsDb {
+	s := []BookingRequest{}
+	return &mockBookingsDb{
+		brs: s,
+	}
+}
+
+func (repo *mockBookingsDb) SaveBooking(br BookingRequest) (id int, err error) {
+	repo.brs = append(repo.brs, br)
+	return len(repo.brs), nil
+}
+
+func (repo *mockBookingsDb) FetchBookings(userID int) ([]BookingRequest, error) {
+	filtered := []BookingRequest{}
+	for i, v := range repo.brs {
+		br := v
+		br.ID = i
+		if br.UserID == userID {
+			filtered = append(filtered, br)
+		}
+	}
+	return filtered, nil
+}
+
+func (repo *mockBookingsDb) FetchUnconfirmedBookings(userID int) ([]BookingRequest, error) {
+	filtered := []BookingRequest{}
+	for i, v := range repo.brs {
+		br := v
+		br.ID = i
+		if br.IsConfirmed == 0 && br.UserID == userID {
+			filtered = append(filtered, br)
+		}
+	}
+	return filtered, nil
+}
+
+func TestBookingTwice(t *testing.T) {
+	mockDb := newBookingsMockDb()
+	serv := NewBookingService(mockDb)
+
+	serv.Book(BookingRequest{
+		UserID: 1,
+	})
+
+	_, err := serv.Book(BookingRequest{
+		UserID: 1,
+	})
+
+	if err != ErrUnconfirmedBookingsExist {
+		t.Error("Same user can book twice")
+	}
+}
