001af88a2877124b6e216150a603b5d6e5c49804
developersam1995 <samrudh.kumar@yulu.bike>
2020-08-02 18:14:58 +0530

    add cabs mock db

--- cmd/http/cabs-handler_test.go
+++ cmd/http/cabs-handler_test.go
@@ -6,19 +6,30 @@ import (
 	"testing"
 
 	cs "github.com/developersam1995/cabs-service/lib/cab-service"
-	"github.com/developersam1995/cabs-service/storage"
 	"github.com/gin-gonic/gin"
 	"github.com/magiconair/properties/assert"
 )
 
 func TestGetCabs(t *testing.T) {
-	newCabs := []cs.cab{
-		cs.Cab
+	newCabs := []cs.Cab{
+		cs.Cab{
+			VehNo: "123",
+			Lat:   12.11,
+			Lon:   66.12,
+		},
+		cs.Cab{
+			VehNo: "124",
+			Lat:   12.11,
+			Lon:   66.12,
+		},
+		cs.Cab{
+			VehNo: "125",
+			Lat:   12.11,
+			Lon:   66.12,
+		},
 	}
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	db := cs.NewCabsMockDb(newCabs)
 
 	r := gin.Default()
 	cs := cs.NewCabsService(db)
@@ -27,7 +38,7 @@ func TestGetCabs(t *testing.T) {
 
 	gw := httptest.NewRecorder()
 
-	req, _ := http.NewRequest("GET", "/cabs?lat=12.23&lon=66.121&dist=500", nil)
+	req, _ := http.NewRequest("GET", "/cabs?lat=12.11&lon=66.12&dist=500", nil)
 	r.ServeHTTP(gw, req)
 	assert.Equal(t, http.StatusOK, gw.Code, "Could not list cabs")
 
