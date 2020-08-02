8489d65379c1a0999faca83fa44f27f36012531f
developersam1995 <samrudh.kumar@yulu.bike>
2020-08-02 13:23:22 +0530

    add config

--- cmd/http/config.go
+++ cmd/http/config.go
@@ -1,15 +1,9 @@
 package main
 
-type dbConfig struct {
-	Host        string `json:"host"`
-	User        string `json:"user"`
-	Password    string `json:"password"`
-	Db          string `json:"db"`
-	Pooling     bool   `json:"pooling"`
-	Connections int    `json:"connections"`
-}
+import "github.com/developersam1995/cabs-service/storage"
+
 type configuration struct {
 	Port          int `json:"port"`
-	Db            dbConfig
+	Db            storage.DbConfig
 	MaxCabGetDist int `json:"max_cab_get_dist"` // See the Get cabs api
 }
--- cmd/http/main.go
+++ cmd/http/main.go
@@ -9,6 +9,7 @@ import (
 	cs "github.com/developersam1995/cabs-service/lib/cab-service"
 	"github.com/developersam1995/cabs-service/storage"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type jsonMock map[string]interface{}
@@ -28,7 +29,7 @@ func init() {
 
 	config = configuration{
 		Port: 8080,
-		Db: dbConfig{
+		Db: storage.DbConfig{
 			Host:     "127.0.0.1",
 			User:     "server_x",
 			Password: "testpassword",
@@ -51,7 +52,10 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	db := storage.New(storage.DbConfig{})
+	db, err := storage.New(config.Db)
+	if err != nil {
+		log.Fatalln("Cannot connect to db:", err)
+	}
 
 	bookingService := cs.NewBookingService(db)
 	cabsService := cs.NewCabsService(db)
